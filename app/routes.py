c878852fd533a9e0ad2e5e49b509d85a3d1622bb
Ryan Sheppard <Rshep3087@gmail.com>
2020-07-11 22:09:51 -0500

    add data post route

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,25 @@
-from flask import render_template
+from dateutil import parser
+
+from flask import render_template, request
 from app import app
+from app import db
+from .models import HomeData
 
 
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template("index.html", title="Home")
+
+
+@app.route("/add_data", methods=["POST"])
+def add_data():
+    home_data = request.get_json()
+    time = parser.parse(home_data["time"])
+
+    new_data = HomeData(temperature=home_data["temperature"], timestamp=time)
+
+    db.session.add(new_data)
+    db.session.commit()
+    return "Done", 201
 
