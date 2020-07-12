80c9271eb31a5a1d51a76c9523b0845136b6b51a
Ryan Sheppard <Rshep3087@gmail.com>
2020-07-12 16:34:31 -0500

    do not parse timestamp

--- app/routes.py
+++ app/routes.py
@@ -30,9 +30,10 @@ def index():
 @app.route("/add_data", methods=["POST"])
 def add_data():
     home_data = request.get_json()
-    time = parser.parse(home_data["time"])
 
-    new_data = HomeData(temperature=home_data["temperature"], timestamp=time)
+    new_data = HomeData(
+        temperature=home_data["temperature"], timestamp=home_data["time"]
+    )
 
     db.session.add(new_data)
     db.session.commit()
