c1b2851d12bef00564bdf7d3a9afb7145bb5011a
Ryan Sheppard <Rshep3087@gmail.com>
2020-07-12 17:29:05 -0500

    new config var name

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "TBkQgbxqSCBKL-SLNA-Q0w"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("HEROKU_POSTGRESQL_CHARCOAL_URL")
     print(SQLALCHEMY_DATABASE_URI)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
